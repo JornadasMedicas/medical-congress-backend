1d2952a02a014bed66def349a2365914afda1f26
PressureDraper <warmarines00@hotmail.com>
2024-10-16 15:07:37 -0600

    add: https cert read

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
 import { Server as HttpServer } from 'http';
+import { createServer } from 'https';
 import cors from 'cors';
 import routerBase from '../routes/base';
 import routeContact from '../routes/contact';
 import routeRegist from '../routes/register';
+import fs from 'fs';
 
 class Server {
     private app: Express;
@@ -16,18 +18,25 @@ class Server {
         this.app = express();
         this.port = parseInt(`${process.env.PORT}`);
 
-        //http server
-        this.server = require('http').createServer(this.app);
+        //http / https server
+        this.server = process.env.ENVIRONMENT == 'productivo'
+            ? createServer(
+                {
+                    cert: fs.readFileSync('/cert/ssaver.gob.mx.crt'),
+                    key: fs.readFileSync('/cert/ssaver.gob.mx.key')
+                }, this.app
+            )
+            : require('http').createServer(this.app);
     }
 
     middlewares() {
-        this.app.use( express.json() );
+        this.app.use(express.json());
         this.app.use(cors({ //only enable cors if clients will connect from remote domains or ports besides 'origin'
             origin: '*', // all domains allowed use *
             methods: ['GET', 'POST'],
             allowedHeaders: ['Content-Type', 'Authorization']
         }));
-        this.app.use( '/', routerBase );
+        this.app.use('/', routerBase);
         this.app.use('/api/contact', routeContact);
         this.app.use('/api/register', routeRegist);
     }
@@ -37,7 +46,9 @@ class Server {
         this.middlewares();
 
         this.server.listen(this.port, () => {
-            console.log(`Server Running on port ${this.port}...`);
+            process.env.ENVIRONMENT == 'productivo'
+                ? console.log(`Server Socket Productivo ready in port: ${this.port}...`)
+                : console.log(`Server Socket Pruebas ready in http://localhost: ${this.port}...`);
         })
     }
 
